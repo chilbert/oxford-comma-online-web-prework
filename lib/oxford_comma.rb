03b4ee503332791c5124685ae5eab7195faac15d
Chase Hilbert <chase@heyspacetime.com>
2019-02-04 22:01:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ elsif array.size == 2
   array.join(" and ")
 elsif array.size == 3
   array_last = array.pop
-   new_array = array.join(",")
+   new_array = array.join(", ")
    new_array << ", and #{array_last}"
  end
  end
