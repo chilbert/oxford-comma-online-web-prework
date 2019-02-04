a22fcc1d6441e79acc6493808a8997ff15a0e2a4
Chase Hilbert <chase@heyspacetime.com>
2019-02-04 22:00:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
-  size = array.size
-  if size == 1
+  if array.size == 1
   array.join
-elsif size == 2
+elsif array.size == 2
   array.join(" and ")
 else
-    array_last = array.pop
+  array_last = array.pop
    new_array = array.join(",")
    new_array << ", and #{array_last}"
  end
