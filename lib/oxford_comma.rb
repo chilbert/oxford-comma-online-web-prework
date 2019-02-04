f42951209f42d3064248e1f91dca5f983226f4c5
Chase Hilbert <chase@heyspacetime.com>
2019-02-04 22:01:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   array.join
 elsif array.size == 2
   array.join(" and ")
-elsif array.size == 3
+else
   array_last = array.pop
    new_array = array.join(", ")
    new_array << ", and #{array_last}"
